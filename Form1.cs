c8f7ac430d63430361ea42c168bb36e3e9c93cc1
soggyburito <stanly.benjamin@gmail.com>
2016-12-20 10:55:14 -0500

    testing2
    testing commits 2

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,7 @@ namespace allMyCircuts
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Text = "Hello";
+            button1.Text = "GoodBye";
         }
     }
 }
